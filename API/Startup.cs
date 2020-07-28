89d07a92da2c4274ac34335db3c659126d7b2710
Duncan Phelps <phelps.duncan@gmail.com>
2020-07-28 14:39:38 -0600

    client side skeleton finished. connected to db. added semantic ui.

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,11 @@ namespace API
             services.AddDbContext<DataContext>(opt =>{
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -47,6 +52,8 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
