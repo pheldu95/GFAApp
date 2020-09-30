b9a7a64663f92f7ac4d088decdacd3791f4be142
Duncan Phelps <phelps.duncan@gmail.com>
2020-09-30 12:16:26 -0500

    fixed register error. i forgot the closing bracket in the password regex in ValidatorExtensions.cs

--- Application/Validators/ValidatorExtension.cs
+++ Application/Validators/ValidatorExtension.cs
@@ -13,7 +13,7 @@ namespace Application.Validators
                 .Matches("[A-Z]").WithMessage("Password must contain 1 uppercase letter")
                 .Matches("[a-z]").WithMessage("Password must have at lease 1 lower case character")
                 .Matches("[0-9]").WithMessage("Must contain a number")
-                .Matches("[^a-zA-Z0-9").WithMessage("Password must contain non alphanumeric");
+                .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain non alphanumeric");
             return options;
         }
 
--- client-app/src/app/common/form/ErrorMessage.tsx
+++ client-app/src/app/common/form/ErrorMessage.tsx
@@ -13,11 +13,9 @@ const ErrorMessage: React.FC<IProps> = ({ error, text }) => {
       <Message.Header>{error.statusText}</Message.Header>
       {error.data && Object.keys(error.data.errors).length > 0 && (
         <Message.List>
-          {Object.values(error.data.errors)
-            .flat()
-            .map((err, i) => (
-              <Message.Item key={i}>error</Message.Item>
-            ))}
+          {Object.values(error.data.errors).flat().map((err, i)=>(
+            <Message.Item key = {i}>{error}</Message.Item>
+          ))}
         </Message.List>
       )}
       {text && <Message.Content content={text} />}
